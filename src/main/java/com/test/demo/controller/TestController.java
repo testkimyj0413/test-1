b13f4544e4b131854b9494e7b3f252fea0d08d73
kimyj <yongjjun8718@gmail.com>
2025-07-20 20:14:40 +0900

    tttt

---
+++ src/main/java/com/test/demo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.test.demo.controller;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+@Profile("dev")
+public class TestController {
+
+
+    @RequestMapping("/info")
+    public String info(){
+        return "app-1-dev";
+    }
+}
