5a8f82daa725212798ae1013b7ddb2ac35de1e72
kimyj <yongjjun8718@gmail.com>
2025-07-22 21:30:05 +0900

    test

--- src/main/java/com/test/demo/controller/TestController.java
+++ src/main/java/com/test/demo/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.test.demo.controller;
 
+import com.test.demo.mapper.TestMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,12 +9,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @ResponseBody
-@Profile("dev")
+//@Profile("dev")
+@RequiredArgsConstructor
 public class TestController {
 
+    private final TestMapper testMapper;
 
     @RequestMapping("/info")
     public String info(){
         return "app-1-dev";
+    }
+
+    @RequestMapping("/db")
+    public String infod(){
+        return testMapper.test().getUserid().toString();
     }
 }
